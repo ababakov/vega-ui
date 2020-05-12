f103bb39f2a526394d7609c5948a0c81b5a24317
Oleg Denisov <c1n1kk@gmail.com>
2020-05-12 15:20:19 +0300

    feature(components): вынести хук в директорию с хуками
    Jira issue: VEGA-44

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -2,3 +2,4 @@ export { usePreviousRef } from './use-previous-ref';
 export { useOnClickOutside } from './use-on-click-outside';
 export { usePortalDomNode } from './use-portal-dom-node';
 export { useKey } from './use-key';
+export { useDraggableContainer } from './use-draggable-container';
